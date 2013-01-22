9f4e6b7c7fbad47d7cc9cfe3d5af95b854a83fc6
Stephen Sawchuk <sawchuk@gmail.com>
2013-01-22 14:32:06 -0500

    dfilter supports object hashes now!

--- src/jquery.data.filter.js
+++ src/jquery.data.filter.js
@@ -1,42 +1,63 @@
 (function($) {
   $.fn.dfilter = function(attribute, value) {
+    var
+      // Stores a reference to the elements that have been matched by the jQuery selector.
+      elems = this,
 
-    // sanitize
-    attribute = attribute.toLowerCase();
-    attribute = attribute.replace(/^data-/, "");
-    attribute = attribute.replace('_', '-');
-    attribute = attribute.replace(' ', '-');
+      // Asks jQuery for the type of the inputted value.
+      valType = $.type(value);
 
-    if (typeof value === "undefined" || value === "") {
-      return this.filter('[data-' + attribute + ']');
+    // Cleans up the attribute to match the format of a data attribute.
+    attribute = attribute.toLowerCase()
+                         .replace(/^data-/, '')
+                         .replace('/[_|\s]/g', '-');
+
+    if (valType === 'undefined' || (valType === 'boolean' && value))
+      // If the value is undefined, empty, or a truthy boolean.
+      return elems.filter('[data-' + attribute + ']');
+
+    if (valType === 'boolean' && !value)
+      // If the value is a falsy boolean.
+      return elems.not('[data-' + attribute + ']');
+
+    if (valType === 'array')
+      // If the value is an array, we build a multiple selector string:
+      //
+      //   .dfilter('coord', [100, 101]) => [data-coord="100"],[data-coord="101"]
+      //
+      // For more on the multiple selector: http://api.jquery.com/multiple-selector/
+      return elems.filter('[data-' + attribute + '="' + value.join('"],[data-' + attribute + '="') + '"]');
+
+    if (valType === 'object') {
+      var
+        // If the value is an object literal, see if we can match on the actual parameter.
+        objectLength = 0,
+
+        // We'll count how many elements match the object literal.
+        matchedLength = 0;
+
+      return elems.filter(function(i, el){
+        objectLength = 0;
+        matchedLength = 0;
+
+        for (var prop in value) {
+          if (!value.hasOwnProperty(prop))
+            return false;
+
+          objectLength++;
+
+          var data = $(el).data(attribute);
+
+          if ($.type(data) === 'object' && data[prop] == value[prop])
+            matchedLength++;
+        }
+
+        return matchedLength == objectLength;
+      });
     }
-    else if (typeof value === "boolean") {
-      if (value) { // value === true
-        return this.filter('[data-' + attribute + ']')
-      }
-      else { // value === false
-        return this.not('[data-' + attribute + ']');
-      }
-    }
-    else if ($.isArray(value)) {
 
-      // In the case that we have an array, we want to build a big multiple selector string:
-      // If we feed in something like:
-      //
-      //   [100, 101]
-      //
-      // Then we build the following selector:
-      //
-      //   [data-id="100"],[data-id="101"]
-      //
-      // For more reading on the multiple selector, see
-      //
-      //   http://api.jquery.com/multiple-selector/
-      return this.filter(
-        '[data-' + attribute + '="' + value.join('"],[data-' + attribute + '="') + '"]'
-      );
-    }
-
-    return this.filter('[data-' + attribute + '="' + value + '"]');
+    // Filters elements based on the number or string value.
+    if (valType === 'string' || valType === 'number')
+      return elems.filter('[data-' + attribute + '="' + value + '"]');
   };
 })(jQuery);
